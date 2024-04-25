2c8ee7265c5478ef29c780c7d58a4f54f345559a
SantiagoLozano16 <167226334+SantiagoLozano16@users.noreply.github.com>
2024-04-25 07:43:58 -0500

    Delete ejercicio2.py

--- ejercicio2.py
+++
@@ -1,6 +0,0 @@
-#Aqui declararé variables 
-a= float (input("digite la altura del rectangulo : "))
-b= float (input("digite la base del rectangulo :"))
-suma=a+b
-# oencontraras tus resultados
-print(f" El perímetro del rectángulo es: {a} el área del rectángulo es: {b}")
