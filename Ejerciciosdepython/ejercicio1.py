93e93f5ca113a536bdfc38ee1581e8815169431c
SantiagoLozano16 <167226334+SantiagoLozano16@users.noreply.github.com>
2024-04-25 08:06:44 -0500

    Add files via upload

---
+++ Ejerciciosdepython/ejercicio1.py
@@ -0,0 +1,25 @@
+a=2
+b=988
+suma =a+b
+print("la suma es ", suma)
+
+a=34
+b=98
+resta =a-b
+print("la resta es ", resta)
+
+a=12
+b=23
+producto =a*b
+print("la operacion es ", producto)
+
+a=23
+b=12
+residuo =a%b
+print("el resultado es",residuo)
+
+a=12
+b=35
+cociente =a/b
+print("el resultado es",cociente)
+
